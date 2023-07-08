3fbf3df7d24c0f64cbce9643b790625cb503bd55
dvtuan228 <tuan.dv.228@gmail.com>
2023-07-08 15:16:04 +0700

    Add loading page

--- src/pages/Loading/index.js
+++ src/pages/Loading/index.js
@@ -1,5 +1,17 @@
+import logo from '../../assets/images/logo.png'
+import './style.scss'
 const Loading = () => {
-    return <div>Loading....</div>
+    return (
+        <div className="loading w-full h-screen bg-white flex items-center justify-center">
+            <div class="load-wrapp">
+                <div class="load-1">
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Loading
